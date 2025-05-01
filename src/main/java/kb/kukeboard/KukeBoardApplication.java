3b9836a56f3512077dd3201de0685bc2de4793e7
wcwdfu <skb9731@gmail.com>
2025-05-01 23:39:14 +0900

    Add snowflake ID generator and test code

---
+++ src/main/java/kb/kukeboard/KukeBoardApplication.java
@@ -0,0 +1,13 @@
+package kb.kukeboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KukeBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KukeBoardApplication.class, args);
+    }
+
+}
