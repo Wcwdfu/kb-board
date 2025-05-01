c055e1a4f9568f6dc150525852a1013d4286fdd3
wcwdfu <skb9731@gmail.com>
2025-05-02 06:45:39 +0900

    implement article CRUD and add test cases

---
+++ service/article/src/test/java/kb/board/article/api/ArticleApiTest.java
@@ -0,0 +1,80 @@
+package kb.board.article.api;
+
+import kb.board.article.service.request.ArticleCreateRequest;
+import kb.board.article.service.response.ArticleResponse;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.junit.jupiter.api.Test;
+import org.springframework.web.client.RestClient;
+
+public class ArticleApiTest {
+    RestClient client = RestClient.create("http://localhost:9000");
+
+    @Test
+    void createTest(){
+        ArticleResponse response = create(new ArticleCreateRequest(
+                "hi", "my content", 1L, 1L
+        ));
+        System.out.println("response = " + response);
+    }
+
+    ArticleResponse create(ArticleCreateRequest request){
+        return client.post()
+                .uri("/v1/articles")
+                .body(request)
+                .retrieve()
+                .body(ArticleResponse.class);
+    }
+
+    @Test
+    void readTest(){
+        ArticleResponse response = read(176357604956528640L);
+        System.out.println("response = " + response);
+    }
+
+    ArticleResponse read(Long articleId){
+        return client.get()
+                .uri("/v1/articles/{articleId}", articleId)
+                .retrieve()
+                .body(ArticleResponse.class);
+    }
+
+    @Test
+    void updateTest(){
+        update(176357604956528640L);
+        ArticleResponse response = read(176357604956528640L);
+        System.out.println("response = " + response);
+    }
+
+    void update(Long articleId) {
+        client.put()
+                .uri("/v1/articles/{articleId}", articleId)
+                .body(new ArticleUpdateRequest("hi 2", "my content 22"))
+                .retrieve()
+                .body(ArticleResponse.class);
+    }
+
+    @Test
+    void deleteTest(){
+        client.delete()
+                .uri("/v1/articles/{articleId}", 176357604956528640L)
+                .retrieve()
+                .body(Void.class);
+    }
+
+    @Getter
+    @AllArgsConstructor
+    static class ArticleCreateRequest {
+        private String title;
+        private String content;
+        private Long writerId;
+        private Long boardId;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    static class ArticleUpdateRequest {
+        private String title;
+        private String content;
+    }
+}
