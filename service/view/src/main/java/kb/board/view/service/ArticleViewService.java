a84935d47c034fa00a16c5dacb68fd555f24c0f4
wcwdfu <skb9731@gmail.com>
2025-05-24 02:58:11 +0900

    조회수 어뷰징 방지 추가

--- service/view/src/main/java/kb/board/view/service/ArticleViewService.java
+++ service/view/src/main/java/kb/board/view/service/ArticleViewService.java
@@ -1,17 +1,28 @@
 package kb.board.view.service;
 
 import kb.board.view.repository.ArticleViewCountRepository;
+import kb.board.view.repository.ArticleViewDistributedLockRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.Duration;
 
 @Service
 @RequiredArgsConstructor
 public class ArticleViewService {
     private final ArticleViewCountRepository articleViewCountRepository;
     private final ArticleViewCountBackUpProcessor articleViewCountBackUpProcessor;
+    private final ArticleViewDistributedLockRepository articleViewDistributedLockRepository;
+
     private static final int BACKUP_BACH_SIZE = 100;
+    private static final Duration TTL = Duration.ofMinutes(10);
 
     public Long increase(Long articleId, Long userId) {
+        if(!articleViewDistributedLockRepository.lock(articleId, userId, TTL)){
+            return articleViewCountRepository.read(articleId);
+        }
+
+
         Long count = articleViewCountRepository.increase(articleId);
         if(count%BACKUP_BACH_SIZE == 0){
             articleViewCountBackUpProcessor.backUp(articleId, count);
